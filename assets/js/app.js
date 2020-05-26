a385b63bbe2a2d128dce816b756abc04bafd8572
allan pedersen <a.pedersen@free.fr>
2020-05-26 13:13:45 +0200

    Add css theme Sketchy from bootswatch.com

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,9 +6,13 @@
  */
 
 // any CSS you import will output into a single css file (app.css in this case)
-import '../css/app.scss';
+// import '../css/app.scss';
+require('../css/app.scss');
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
 // import $ from 'jquery';
+var $ = require('jquery');
+
+require('bootstrap');
 
 console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
